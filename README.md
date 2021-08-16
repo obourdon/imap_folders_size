a1d1998dd3055c64e696c0c333807546e0204738
Olivier Bourdon <olivier.bourdon@squarescale.com>
2021-08-16 10:59:36 +0200

    Python3 1st working version
    Changing execution rights for the Python script
    Now using PyCharm from Jetbrains to ease debugging
    
    TODOs:
    - add command line arguments for properly providing informations
    without any hardcoding.
    - retrieve quotas if IMAP server supports them
    - provide several output sorting methods

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # imap_folders_size
 List all IMAP folders with size in number of messages and size in bytes
+
+To use, followthe instructions hereafter:
+
+```
+# Create Python virtual environment
+$ python3 -m venv ~/.venvs/imap
+# Activate this environment
+$ . ~/.venvs/imap/bin/activate
+# Install Python packages requirements
+$ pip3 install -r requirements.txt
+# Optionally update pip3
+$ python3 -m pip install --upgrade pip
+# Run the program using the proper email account
+$ LOGNAME=myemail-without-the@free.fr ./imap_folders_size.py
+```
--- imap_folders_size.py
+++ imap_folders_size.py
@@ -1,73 +1,97 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
 
 # LOGNAME=my-email
+# LOGPASSWD=xxxxx
+import sys, os, imaplib, imapclient, getpass, re, pdb
 
-import sys, os, string, imaplib, getpass, pdb
-
-def print_folder_size(folder, nb, size):
-	fmt = "%16s %-50s %5s"
-	print fmt % (size, folder, nb)
-
-def folder_size(M, folder_entry):
-	fs = 0
-	nb = '0'
-	folder_items = folder_entry.split()
-	# Select the desired folder
-	mbx = string.join(folder_items[2:])
-	pdb.set_trace()
-	result, nb = M.select(mbx, readonly=1)
-	if result != 'OK':
-		print 'IMAP folder select returned %s' % (result)
-		return -1, 0
-	# Go through all the messages in the selected folder
-	typ, msg = M.search(None, 'ALL')
-	# Find the first and last messages
-	m = [int(x) for x in msg[0].split()]
-	if m:
-		m.sort()
-		msgset = "%d:%d" % (m[0], m[-1])
-		result, msizes = M.fetch(msgset, "(UID RFC822.SIZE)")
-		if result != 'OK':
-			print 'IMAP messages sizes returned %s' % (result)
-			return -1, 0
-		for msg in msizes:
-			fs += int(msg.split()[-1].replace(')', ''))
-	print_folder_size(mbx, int(nb[0]), fs) 
-	return int(nb[0]), fs
+imap_folder_re = re.compile(r"^\([^)]*\) (.*)$")
 
 imap_server = "imap.free.fr"
 #imap_server = "imap.gmail.com"
 
-# Open a connection to the IMAP server using SSL and proper port
-M = imaplib.IMAP4_SSL(imap_server, 993)
-# User is retrieved from LOGNAME environment variable
-# password is asked on command line
-(user, passwd) = (getpass.getuser(), getpass.getpass('Enter password for user %s > ' % os.environ['LOGNAME']))
 
-try:
-	M.login(user, passwd)
-except imaplib.IMAP4.error as e:
-	print 'Error: %s' % (e)
-	sys.exit(1)
+def print_folder_size(folder, nb, size):
+    fmt = "%16s %-50s %5s"
+    print(fmt % (size, folder, nb))
 
-# The list of all folders 
-result, folders = M.list()
-if result != 'OK':
-	print 'IMAP folder list returned %s' % (result)
-	sys.exit(1)
 
-print_folder_size("Folder", "# Msg", "Size") 
+def folder_size(M, folder_entry):
+    fs = 0
+    nb = '0'
+    imap_folder_match = imap_folder_re.match(str(folder_entry, 'utf-8'))
+    if imap_folder_match == None:
+        print('IMAP folder %s does not match regexp' % (folder_entry))
+        return 0, 0
+    folder_items = imap_folder_match.group(1).split()
+    # Select the desired folder
+    mbx = '"' + ' '.join(map(lambda x: x.replace('"', ''), folder_items[1:])) + '"'
+    result, nb = M.select(mbx, readonly=1)
+    if result != 'OK':
+        print('%s IMAP folder select returned %s' % (mbx, result))
+        return -1, 0
+    # Go through all the messages in the selected folder
+    typ, msg = M.search(None, 'ALL')
+    # Find the first and last messages
+    m = [int(x) for x in msg[0].split()]
+    if m:
+        m.sort()
+        msgset = "%d:%d" % (m[0], m[-1])
+        result, msizes = M.fetch(msgset, "(RFC822.SIZE)")
+        if result != 'OK':
+            print('IMAP messages sizes returned %s' % (result))
+            return -1, 0
+        for msg in msizes:
+            fs += int(str(msg.split()[-1], 'utf-8').replace(')', ''))
+    print_folder_size(imapclient.imap_utf7.decode(mbx.encode()).strip('"'), int(nb[0]), fs)
+    return int(nb[0]), fs
 
-nmessages_total = 0
-size_total = 0
 
-#pdb.set_trace()
-for folder in folders:
-	fcount, fsize = folder_size(M, folder)
-	nmessages_total += fcount
-	size_total += fsize
+def env_or_tty_passwd():
+    lpasswd = os.getenv("LOGPASSWD")
+    if lpasswd == None:
+        lpasswd = getpass.getpass('Enter password for user %s > ' % os.environ['LOGNAME'])
+    return lpasswd
 
-print_folder_size("Sum", nmessages_total, size_total) 
 
-# Close the connection 
-M.logout() 
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Open a connection to the IMAP server using SSL and proper port
+    M = imaplib.IMAP4_SSL(imap_server, 993)
+    # User is retrieved from LOGNAME environment variable
+    # password is asked on command line or environment variable
+    (user, passwd) = (getpass.getuser(), env_or_tty_passwd())
+
+    try:
+        M.login(user, passwd)
+    except imaplib.IMAP4.error as e:
+        print('Error: %s' % (e))
+        sys.exit(1)
+
+    # The list of all folders
+    result, folders = M.list()
+    if result != 'OK':
+        print('IMAP folder list returned %s' % (result))
+        sys.exit(1)
+
+    print_folder_size("Folder", "# Msg", "Size")
+
+    nmessages_total = 0
+    size_total = 0
+
+    #pdb.set_trace()
+    for folder in folders:
+        fcount, fsize = folder_size(M, folder)
+        nmessages_total += fcount
+        size_total += fsize
+
+    print_folder_size("Sum", nmessages_total, size_total)
+
+    # Close the connection
+    M.logout()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
