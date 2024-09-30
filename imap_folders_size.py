93214296455ddd253d3913d6133afb9a33c92162
Olivier Bourdon <olivier.bourdon@squarescale.com>
2024-09-30 16:57:03 +0200

    Rewrite using type hints and modularity
    Better exception handling
    Also try to conform to PEP8/Flake8

--- imap_folders_size.py
+++ imap_folders_size.py
@@ -1,29 +1,51 @@
 #!/usr/bin/env python3
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 # LOGNAME=my-email
 # LOGPASSWD=xxxxx
 
-import email, getpass, imapclient, imaplib, os, re, sys, tabulate
-from datetime import datetime
-import numpy as np
+import pdb
 
+from datetime import datetime
+import email
+import getpass
+import imapclient
+import imaplib
+import numpy as np
+import os
+import re
+import sys
+import tabulate
+
+
+# Some regular expressions for IMAP response decoding
 imap_folder_re = re.compile(r"^\([^)]*\) (.*)$")
 imap_quota_re = re.compile(r"^\"[^\"]*\" \(STORAGE (\d+) (\d+)\)$")
-special_folder_flags = set(('Noselect', 'All', 'Important'))
-known_folder_flags = set(('HasNoChildren', 'HasChildren', 'Drafts', 'Sent', 'Junk', 'Trash', 'Flagged'))
+
+# Define some constant for IMAP folders flags
+special_folder_flags = set((
+    'Noselect',
+    'All',
+    'Important'
+    ))
+known_folder_flags = set((
+    'HasNoChildren',
+    'HasChildren',
+    'Drafts',
+    'Sent',
+    'Junk',
+    'Trash',
+    'Flagged'))
 known_folder_flags.update(special_folder_flags)
-imap_server = "imap.free.fr"
-#imap_server = "imap.gmail.com"
+
+# Other globalss
+# imap_server = "imap.free.fr"
+imap_server = "imap.gmail.com"
 
 
 def trace_msg(msg):
     if os.getenv("NO_TRACE"):
         return
-    print(msg, datetime.now().strftime("%H:%M:%S"))
+    print(msg)
 
 
 def human_readable_size(size, suffix='B', decimal_places=1, units_offset=0):
@@ -42,17 +64,17 @@ def folder_real_name(folder, decoded=True):
 
 
 def list_append(v, name="size", extras={}):
-    cmd="message_" + name + "s.append({**extras, **{'VALUE': v}})"
+    cmd = "message_" + name + "s.append({**extras, **{'VALUE': v}})"
     eval(cmd)
 
 
 def message_subject_from_to(msg):
     msg_id = msg.get('ID')
     mbx = msg.get('FOLDER')
-    if mbx == None or msg_id == None:
+    if not mbx or not msg_id:
         print(f"Unable to retrieve folder for message {msg.get('ID')} in {msg.get('FOLDER')}")
         return "(None)", "(None)", "(None)"
-    result, nb = M.select(mbx, readonly=1)
+    result, nb = XXX.select(mbx, readonly=1)
     if result != 'OK':
         print(f"{mbx} IMAP folder select returned {result} (message_subject_from_to)")
         return "(None)", "(None)", "(None)"
@@ -77,16 +99,18 @@ def message_subject_from_to(msg):
     return "(None)", "(None)", "(None)"
 
 
-def folder_size(M, folder_entry):
+def folder_size(
+    cnx: imaplib.IMAP4_SSL,
+    folder_entry: bytes,
+        ) -> tuple[dict[str, str | int], Exception]:
     global message_sizes, message_dates, min_max
     fs = 0
     nb = '0'
     # folder_entry.decode().split(' "/" ')
     # 2 element tuple
     imap_folder_match = imap_folder_re.match(str(folder_entry, 'utf-8'))
-    if imap_folder_match == None:
-        print(f"IMAP folder {folder_entry} does not match regexp")
-        return {}
+    if not imap_folder_match:
+        return {}, Exception(f"IMAP folder {folder_entry} does not match regexp")
     folder_items = imap_folder_match.group(1).split()
     # str(folder_entry, 'utf-8').split(' "/" ') same as folder_entry.decode().split(' "/" ')
     folder_flags = eval(','.join(folder_entry.decode().split(' "/" ')[0].replace('\\','').split(' ')).replace('(','("').replace(',','","').replace(')','",)'))
@@ -96,27 +120,31 @@ def folder_size(M, folder_entry):
     mbx = '"' + ' '.join(map(lambda x: x.strip('"'), folder_items[1:])) + '"'
     # Folder is not selectable or is tagged with special meaning
     if len(special_folder) > 0:
-        print(f"{mbx} IMAP folder not processed {special_folder} (folder_size)")
-        return {}
+        return {}, Exception(f"{mbx} IMAP folder not processed {special_folder} (folder_size)")
     unknown_folder_flags = s1.difference(known_folder_flags)
+    # TODO: see how to report this better upstream
     if len(unknown_folder_flags) > 0:
         print(f"{mbx} IMAP folder got unknown flag(s) -> {unknown_folder_flags}")
     # Select the desired folder
-    result, nb = M.select(mbx, readonly=1)
+    result, nb = cnx.select(mbx, readonly=1)
     if result != 'OK':
-        print(f"{mbx} IMAP folder select returned {result} (folder_size)")
-        return {}
+        return {}, Exception(f"{mbx} IMAP folder select returned {result} (folder_size)")
+    # TODO: may be not call search if int(nb[0]) == 0
+    # and/or verify that int(nb[0]) == len(msg[0].split())
     # Go through all the messages in the selected folder
-    typ, msgs = M.search(None, 'ALL')
+    typ, msgs = cnx.search(None, 'ALL')
+    # TODO: verify that typ == 'OK'
     # Find the first and last messages
     m = [int(x) for x in msgs[0].split()]
     if m:
         m.sort()
         msgset = f"{m[0]}:{m[-1]}"
-        result, msizes = M.fetch(msgset, "(INTERNALDATE RFC822.SIZE)")
+        result, msizes = cnx.fetch(msgset, "(INTERNALDATE RFC822.SIZE)")
         if result != 'OK':
-            print(f"IMAP messages sizes returned {result}")
-            return {}
+            return {}, Exception(f"IMAP messages sizes returned {result}")
+        # TODO: check that len(msizes) == int(nb[0])
+        # TODO: may be find a more clever way to properly compute the
+        # size and date whatever the order they are returned in
         for msg in map(
             lambda x: dict(
                 list(
@@ -124,99 +152,169 @@ def folder_size(M, folder_entry):
                         *[iter(re.sub(
                             r'([1-9][0-9]*) \((.*)\)', r'ID,\1,\2',
                             str(
-                                x.replace(b'"',b'')
+                                x.replace(b'"', b'')
                                 .replace(b' RFC822.SIZE ', b',SIZE,')
                                 .replace(b' INTERNALDATE ', b',DATE,')
                                 .replace(b'RFC822.SIZE ', b'SIZE,')
-                                .replace(b'INTERNALDATE ', b'DATE,'), 'utf-8'))
-                            .split(','))] * 2
+                                .replace(b'INTERNALDATE ', b'DATE,'), 'utf-8')
+                            ).split(','))] * 2
                         )
                     )
                 ),
-            msizes):
+            msizes
+        ):
             msg_size = int(msg['SIZE'])
             msg_date = None
             try:
-              msg_date = datetime.strptime(msg['DATE'], '%d-%b-%Y %H:%M:%S %z')
-              list_append(msg_date, name="date", extras={'ID': int(msg['ID']), 'FOLDER': mbx, 'SIZE': msg_size})
+                msg_date = datetime.strptime(
+                    msg['DATE'],
+                    '%d-%b-%Y %H:%M:%S %z'
+                    )
+                list_append(
+                    msg_date,
+                    name="date",
+                    extras={
+                        'ID': int(msg['ID']),
+                        'FOLDER': mbx,
+                        'SIZE': msg_size
+                        })
             except ValueError as e:
+                # TODO: see hoe to report this better upstream
                 print(f"IMAP message date decoding error: {msg[1]} {e}")
-            list_append(msg_size, name="size", extras={'ID': int(msg['ID']), 'FOLDER': mbx, 'DATE': msg_date})
+            list_append(
+                msg_size,
+                name="size",
+                extras={
+                    'ID': int(msg['ID']),
+                    'FOLDER': mbx,
+                    'DATE': msg_date
+                    })
             fs += msg_size
-    return {'name': folder_real_name(mbx.strip('"')), 'messages': int(nb[0]), 'size': fs}
+    return {
+        'name': folder_real_name(mbx.strip('"')),
+        'messages': int(nb[0]),
+        'size': fs
+        }, None
 
 
-def env_or_tty_passwd():
-    return os.getenv("LOGPASSWD") or getpass.getpass(f"Enter password for user {os.environ['LOGNAME']} > ")
+def env_or_tty_passwd() -> str:
+    return os.getenv("LOGPASSWD") or getpass.getpass(
+        f"Enter password for user {os.environ['LOGNAME']} > "
+        )
+
+
+def login(
+    svr: str,
+    port: int = 993,
+    user: str = None,
+    password: str = None,
+        ) -> imaplib.IMAP4_SSL:
+    # Open a connection to the IMAP server using SSL and proper port
+    cnx = imaplib.IMAP4_SSL(svr, port)
+    try:
+        cnx.login(user, password)
+    except imaplib.IMAP4.error as e:
+        # TODO: new exception type inheriting from IMAP4
+        raise imaplib.IMAP4.error(f"IMAP Login error: {e}")
+    except Exception as e:
+        raise Exception(f"IMAP Login error. Unknown exception: {e}")
+    return cnx
+
+
+def get_quotas(
+    cnx: imaplib.IMAP4_SSL,
+        ) -> tuple[int, int]:
+    try:
+        # List server capabilities
+        capabilities_rsp = cnx.capability()
+        if (capabilities_rsp[0] != 'OK'):
+            raise Exception('Unable to retrieve IMAP server capabilities')
+        if " QUOTA " in str(capabilities_rsp[1][0]):
+            quota_rsp = cnx.getquotaroot("INBOX")
+            if (quota_rsp[0] != 'OK'):
+                raise Exception('Unable to retrieve IMAP server quotas')
+            quota_infos = imap_quota_re.match(str(quota_rsp[1][1][0], 'utf-8'))
+            if not quota_infos:
+                raise Exception('Unable to parse IMAP server quotas')
+            return int(quota_infos.group(1)), int(quota_infos.group(2))
+    except imaplib.IMAP4.error as e:
+        raise Exception(f"IMAP Quotas erro: {e}")
+    except Exception as e:
+        raise Exception(f"IMAP Quotas error. Unknown exception: {e}")
+
+
+def get_folders(
+    cnx: imaplib.IMAP4_SSL,
+        ) -> list[bytes]:
+    # The list of all folders
+    result, folders = cnx.list()
+    if result != 'OK':
+        raise Exception(f"IMAP folder list returned {result}")
+    return folders
+
+
+def error_or_warning(cond: bool) -> str:
+    if cond:
+        return "ERROR"
+    return "WARNING"
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    trace_msg('STARTING')
-    # Open a connection to the IMAP server using SSL and proper port
-    M = imaplib.IMAP4_SSL(imap_server, 993)
-    # User is retrieved from LOGNAME environment variable
-    # password is asked on command line or environment variable
-    (user, passwd) = (getpass.getuser(), env_or_tty_passwd())
-
     try:
-        M.login(user, passwd)
+        # User is retrieved from LOGNAME environment variable
+        # password is asked on command line or environment variable
+        (usr, passwd) = (getpass.getuser(), env_or_tty_passwd())
+        cnx = login(imap_server, user=usr, password=passwd)
+        quota_used, quota_total = get_quotas(cnx)
+        folders = get_folders(cnx)
     except imaplib.IMAP4.error as e:
-        print(f"IMAP Login error: {e}")
-        sys.exit(1)
-
-    # List server capabilities
-    try:
-        capabilities_rsp = M.capability()
-        if (capabilities_rsp[0] != 'OK'):
-            raise Exception('Unable to retrieve IMAP server capabilities')
-        if " QUOTA " in str(capabilities_rsp[1][0]):
-            quota_rsp = M.getquotaroot("INBOX")
-            if (quota_rsp[0] != 'OK'):
-                raise Exception('Unable to retrieve IMAP server quotas')
-            quota_infos = imap_quota_re.match(str(quota_rsp[1][1][0], 'utf-8'))
-            if quota_infos == None:
-                raise Exception('Unable to parse IMAP server quotas')
-            quota_used, quota_total = int(quota_infos.group(1)), int(quota_infos.group(2))
-    except imaplib.IMAP4.error as e:
-        print(f"IMAP capabilities error: {e}")
+        print(f"IMAP error: {e}")
         sys.exit(1)
     except Exception as e:
-        print(f"IMAP capabilities error: {e}")
-
-    trace_msg('QUOTAS ACQUIRED')
-    # The list of all folders
-    result, folders = M.list()
-    if result != 'OK':
-        print(f"IMAP folder list returned {result}")
-        sys.exit(1)
+        print(f"Exception error: {e}")
+        sys.exit(2)
 
     nmessages_total = 0
     size_total = 0
 
     imap_folders = []
-    min_max = {'size_min': None, 'size_max': None, 'date_min': None, 'date_max': None}
+    min_max = {
+        'size_min': None,
+        'size_max': None,
+        'date_min': None,
+        'date_max': None
+        }
     message_sizes = []
     message_dates = []
     trace_msg('FOLDERS ACQUIRED')
     for folder in folders:
-        folder_infos = folder_size(M, folder)
+        folder_infos, ex = folder_size(cnx, folder)
+        if ex:
+            print(f'{error_or_warning(len(folder_infos) > 0)}: got {ex}')
         if folder_infos.get('name'):
-            folder_stats = [folder_infos['name'], folder_infos['messages'], folder_infos['size']]
-            if quota_used != None and quota_used != 0:
-                folder_stats.append((100.0 * folder_infos['size']) / (1024 * quota_used))
+            folder_stats = [
+                folder_infos['name'],
+                folder_infos['messages'],
+                folder_infos['size']
+                ]
+            if quota_used:
+                folder_stats.append(
+                    (100.0 * folder_infos['size'])
+                    / (1024 * quota_used))
             imap_folders.append(folder_stats)
             nmessages_total += imap_folders[-1][1]
             size_total += imap_folders[-1][2]
-
+    pdb.set_trace()
+    trace_msg('FOLDERS PROCESSED')
     summary = ["Sum", nmessages_total, size_total]
     hfields = ["Folder", "# Msg", "Size"]
-    if quota_used != None and quota_used != 0:
+    if quota_used:
         hfields.append("%")
         summary.append(100)
     imap_folders.append(summary)
     print(tabulate.tabulate(imap_folders, headers=hfields, floatfmt=".2f"))
-    if quota_used != None and quota_total != None:
+    if quota_used and quota_total:
         print(f"\nQuotas Used: {human_readable_size(quota_used*1024)} Total: {human_readable_size(quota_total*1024)} Usage: {(100*quota_used)/quota_total:.2f}%")
     trace_msg('BASIC STATS PRINTED')
     sdata = np.array(list(map(lambda x: x.get("VALUE"), message_sizes)))
@@ -235,7 +333,4 @@ if __name__ == '__main__':
     print(tabulate.tabulate(biggest, headers=["ID", "Size", "%", "Date", "Folder", "From", "Subject"], floatfmt=".2f"))
     print(f"\nYou can save {human_readable_size(to_save)} ({((100*to_save)/(1024*quota_used)):.2f}%) by cleaning up the {len(big_messages)} biggest messages\n")
     # Close the connection
-    M.logout()
-    trace_msg('DETAILED STATS PRINTED')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    cnx.logout()
